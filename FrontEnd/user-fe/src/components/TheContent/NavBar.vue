045cc08bbd7a1865841b86653df8c75de2bbf78f
HaiLong <hnlong2002@gmail.com>
2023-05-25 20:55:32 +0700

    feat: init frontend

---
+++ FrontEnd/user-fe/src/components/TheContent/NavBar.vue
@@ -0,0 +1,66 @@
+<template>
+    <div class="navbar"
+    data-aos="flip-left"
+     data-aos-easing="ease-out-cubic"
+     data-aos-duration="1000">
+        <div class="navbar__title">
+            <i class="fas fa-bars" ></i>
+            <span style="font-size:20px; font-weight:500"
+            data-aos="fade-right"
+            data-aos-duration="2000">DANH MỤC SẢN PHẨM</span>
+        </div>
+        <div class="navbar__item" v-for="item in list" :key="item.id" @click="$emit('changeCategory',item)"
+        data-aos="fade-left"
+        data-aos-duration="1500">{{item.name}}</div>
+    </div>
+</template>
+<script>
+import api from '@/js/api'
+export default {
+    data() {
+        return {
+            list:[]
+        }
+    },
+    created(){
+        this.getItems();
+    },
+    methods:{
+        getItems(){
+            this.$axios.get(api.CategoryApi).then(res=> {
+                this.list = res.data;
+            });
+        }
+    }
+}
+</script>
+<style scoped>
+.navbar{
+    min-width: 300px;
+    border: 1px solid #ccc;
+    border-radius: 8px;
+    background: #fff;
+    height: 100%;
+}
+.navbar__title{
+    display: flex;
+    gap: 12px;
+    height: 50px;
+    padding: 0 24px;
+    align-items: center;
+}
+.navbar__item{
+    display: block;
+    width: 100%;
+    height: 50px;
+    line-height: 50px;
+    text-align: left;
+    padding: 0 24px;
+    border-top: 1px solid #ccc;
+    transition: all linear 1s;
+    cursor: pointer;
+}
+.navbar__item:hover{
+    background: #eee;
+}
+</style>
---
+++ FrontEnd/user-fe/src/components/TheContent/TopNavBar.vue
@@ -0,0 +1,61 @@
+<template>
+    <div class="top-navbar">
+        <router-link to="/content" class="top-navbar__item"
+        data-aos="fade-left"
+        data-aos-duration="1000">Trang chủ</router-link>
+        <a href="#" class="top-navbar__item"
+        data-aos="fade-left"
+        data-aos-duration="1400">Giới thiệu</a>
+        <a href="#" class="top-navbar__item"
+        data-aos="fade-left"
+        data-aos-duration="1800">Liên hệ</a>
+        <a href="#" class="top-navbar__item"
+        data-aos="fade-left"
+        data-aos-duration="2100">Hỗ trợ</a>
+        <a href="#" class="top-navbar__item"
+        data-aos="fade-left"
+        data-aos-duration="2400">Dịch vụ</a>
+        <a href="#" class="top-navbar__item"
+        data-aos="fade-left"
+        data-aos-duration="2700">Chính sách</a>
+        <a href="#" class="top-navbar__item"
+        data-aos="fade-left"
+        data-aos-duration="3000">Câu hỏi thường gặp</a>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style scoped>
+.top-navbar{
+    display: flex;
+    padding: 0 48px;
+    justify-content: flex-start;
+    align-items: center;
+    gap: 4px;
+    background: rgb(238,174,202);
+background: linear-gradient(291deg, rgba(238,174,202,1) 0%, rgba(148,187,233,1) 100%);
+}
+.top-navbar__item{
+    height: 40px;
+    min-width: 100px;
+    padding: 0 12px;
+    display: block;
+    line-height: 40px;
+    text-align: center;
+    color: #000;
+    font-size: 20px;
+    border: 1px solid transparent;
+    border-radius: 4px;
+    font-weight: 600;
+    transition: all linear 0.5s;
+}
+.top-navbar__item:hover{
+    color: #fff;
+    background: rgb(131,58,180);
+background: linear-gradient(90deg, rgba(131,58,180,1) 0%, rgba(253,29,29,1) 50%, rgba(252,176,69,1) 100%);
+    border: 1px solid ;
+}
+</style>
